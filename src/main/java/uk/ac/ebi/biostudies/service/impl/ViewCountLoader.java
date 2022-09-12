43fca31201dda66c135202fe436f76df60cd12d5
behrangi <ehsan.behrangi@gmail.com>
2022-09-12 22:33:38 +0100

    Update UI to consume the new VIEWS stats collection [#183094673]

---
+++ src/main/java/uk/ac/ebi/biostudies/service/impl/ViewCountLoader.java
@@ -0,0 +1,43 @@
+package uk.ac.ebi.biostudies.service.impl;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import uk.ac.ebi.biostudies.api.util.Constants;
+import uk.ac.ebi.biostudies.config.IndexConfig;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Scope("singleton")
+public class ViewCountLoader {
+    private static final Logger LOGGER = LogManager.getLogger(ViewCountLoader.class);
+
+    private static final Map<String, Long> ACCESSION_VIEW_COUNT_MAP = new HashMap();
+    @Autowired
+    IndexConfig indexConfig;
+
+    public static Map<String, Long> getViewCountMap() {
+        return ACCESSION_VIEW_COUNT_MAP;
+    }
+
+    public static void freeMemoryOfViewCountMap() {
+        ACCESSION_VIEW_COUNT_MAP.clear();
+    }
+
+    public void loadViewCountFile() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(indexConfig.getViewCountInputFile()))) {
+            for (String line; (line = bufferedReader.readLine()) != null; ) {
+                String[] tokens = line.split(",");
+                ACCESSION_VIEW_COUNT_MAP.put(tokens[0], Long.valueOf(tokens[1]));
+            }
+        } catch (Exception exception) {
+            LOGGER.error("problem in parsing view count file: {} ", Constants.VIEW_COUNT_CSV, exception);
+        }
+    }
+}
