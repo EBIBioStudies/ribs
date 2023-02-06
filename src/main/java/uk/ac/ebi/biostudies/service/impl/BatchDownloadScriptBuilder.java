6540bcbe2e65ac8c0988b1473f29e3982ab1b90c
Awais Athar <awais@ebi.ac.uk>
2023-02-06 12:59:50 +0000

    All NFS/FIRE downloads for aspera

--- src/main/java/uk/ac/ebi/biostudies/service/impl/BatchDownloadScriptBuilder.java
+++ src/main/java/uk/ac/ebi/biostudies/service/impl/BatchDownloadScriptBuilder.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import uk.ac.ebi.biostudies.api.util.Constants;
 
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,53 +19,55 @@ import java.util.stream.Collectors;
 public class BatchDownloadScriptBuilder {
     private static final Logger LOGGER = LogManager.getLogger(BatchDownloadScriptBuilder.class.getName());
     static Map<String, String> DownloadTemplates = new HashMap<>();
-    static{
+
+    static {
         loadDlTemplates();
     }
-    public String fillTemplate(String downloadType, List<String> fileNames, String baseDirectory, String os, Constants.File.StorageMode storageMode){
-        String content="";
-        try{
+
+    private static void loadDlTemplates() {
+        String[] fileNames = {"aspera-unix", "aspera-windows", "ftp-unix", "ftp-windows"};
+        for (String fName : fileNames) {
+            try {
+                InputStream templateStream = new ClassPathResource("batchdl/" + fName).getInputStream();
+                String fileTemplate = IOUtils.toString(templateStream, StandardCharsets.UTF_8);
+                DownloadTemplates.put(fName, fileTemplate);
+                templateStream.close();
+            } catch (Exception ex) {
+                LOGGER.error("Cant open download template file {}", fName, ex);
+            }
+        }
+    }
+
+    public String fillTemplate(String downloadType, List<String> fileNames, String baseDirectory, String os, Constants.File.StorageMode storageMode) {
+        String content = "";
+        try {
             String fileTemplate = DownloadTemplates.get(getTemplate(downloadType, os));
             content = fillFileTemplate(fileTemplate, fileNames, baseDirectory, downloadType, storageMode);
-            if(!os.equalsIgnoreCase(Constants.OS.WINDOWS))
-                content = "#!/bin/bash\r\n"+content;
-        }catch (Exception ex){
+            if (!os.equalsIgnoreCase(Constants.OS.WINDOWS))
+                content = "#!/bin/bash\r\n" + content;
+        } catch (Exception ex) {
             LOGGER.error("Cant open download template file {}", getTemplate(downloadType, os), ex);
         }
         return content;
     }
-    String getTemplate(String type, String os){
+
+    String getTemplate(String type, String os) {
         String myOs = os;
-        if(!myOs.equalsIgnoreCase(Constants.OS.WINDOWS))
-            myOs=Constants.OS.UNIX;
+        if (!myOs.equalsIgnoreCase(Constants.OS.WINDOWS))
+            myOs = Constants.OS.UNIX;
         StringBuilder result = new StringBuilder(type);
         result.append("-").append(myOs);
         return result.toString();
     }
 
-    String fillFileTemplate(String fileTemplate, List<String> fileNames, String baseDirectory, String downloadType, Constants.File.StorageMode storageMode){
+    String fillFileTemplate(String fileTemplate, List<String> fileNames, String baseDirectory, String downloadType, Constants.File.StorageMode storageMode) {
         String content = "";
-        if(downloadType.equalsIgnoreCase("ftp")){
-            String allFiles = fileNames.stream().map(name -> "mget \""+name+"\"").collect(Collectors.joining("\r\n"));
+        if (downloadType.equalsIgnoreCase("ftp")) {
+            String allFiles = fileNames.stream().map(name -> "mget \"" + name + "\"").collect(Collectors.joining("\r\n"));
             content = String.format(fileTemplate, storageMode.toString().toLowerCase() + "/" + baseDirectory, allFiles);
-        }
-        else if(downloadType.equalsIgnoreCase("aspera")){
-            content = fileNames.stream().map(name -> String.format(fileTemplate, "\""+baseDirectory+"/Files/"+name+"\"")).collect(Collectors.joining("\r\n"));
+        } else if (downloadType.equalsIgnoreCase("aspera")) {
+            content = fileNames.stream().map(name -> String.format(fileTemplate, "\"" + storageMode.toString().toLowerCase() + "/" + baseDirectory + "/Files/" + name + "\"")).collect(Collectors.joining("\r\n"));
         }
         return content;
-    }
-
-    private static void loadDlTemplates(){
-        String fileNames[]= {"aspera-unix", "aspera-windows", "ftp-unix", "ftp-windows"};
-        for(String fName:fileNames){
-            try {
-                InputStream templateStream = new ClassPathResource("batchdl/" + fName).getInputStream();
-                String fileTemplate = IOUtils.toString(templateStream, "UTF-8");
-                DownloadTemplates.put(fName, fileTemplate);
-                templateStream.close();
-            }catch (Exception ex){
-                LOGGER.error("Cant open download template file {}", fName, ex);
-            }
-        }
     }
 }
