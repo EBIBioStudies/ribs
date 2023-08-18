989a4128d4269b1eab6b2a2e72c45f8f36342bb8
ehsan <ehsan@ebi.ac.uk>
2023-08-18 17:54:43 +0100

    Create a flag to make customizable ftp redirecting [#185862279]

--- src/main/java/uk/ac/ebi/biostudies/service/file/filter/FtpRedirectFilter.java
+++ src/main/java/uk/ac/ebi/biostudies/service/file/filter/FtpRedirectFilter.java
@@ -1,15 +1,21 @@
 package uk.ac.ebi.biostudies.service.file.filter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 import uk.ac.ebi.biostudies.api.util.StudyUtils;
 import uk.ac.ebi.biostudies.service.file.FileMetaData;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Component
 public class FtpRedirectFilter implements FileChainFilter{
+    @Autowired
+    private Environment env;
     @Override
     public boolean handleFile(FileMetaData fileMetaData, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        if (fileMetaData.isPublic() && !StudyUtils.isPageTabFile(fileMetaData.getAccession(), fileMetaData.getUiRequestedPath()) ) {
+        if (env.getProperty("fire.s3.ftp.redirect.enable", Boolean.class, true) && fileMetaData.isPublic() && !StudyUtils.isPageTabFile(fileMetaData.getAccession(), fileMetaData.getUiRequestedPath()) ) {
             response.sendRedirect( "https://ftp.ebi.ac.uk/biostudies/"+ fileMetaData.getStorageMode().toString().toLowerCase() +"/" + fileMetaData.getRelativePath() + "/Files/" + fileMetaData.getUiRequestedPath() );
             return true;
         }
