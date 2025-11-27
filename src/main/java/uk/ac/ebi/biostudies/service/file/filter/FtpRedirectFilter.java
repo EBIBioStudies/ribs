39ebedb6937a56083e18ebe7dcf031997aafefb3
Mauricio Martinez Jimenez <mmartinez@ebi.ac.uk>
2025-11-27 11:17:28 +0000

    Format changes

--- src/main/java/uk/ac/ebi/biostudies/service/file/filter/FtpRedirectFilter.java
+++ src/main/java/uk/ac/ebi/biostudies/service/file/filter/FtpRedirectFilter.java
@@ -10,18 +10,29 @@ import uk.ac.ebi.biostudies.config.IndexConfig;
 import uk.ac.ebi.biostudies.service.file.FileMetaData;
 
 @Component
-public class FtpRedirectFilter implements FileChainFilter{
-    @Autowired
-    private FireConfig fireConfig;
-    @Autowired
-    private IndexConfig indexConfig;
-    @Override
-    public boolean handleFile(FileMetaData fileMetaData, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        if (fireConfig.isFtpRedirectEnabled() && fileMetaData.isPublic() && !StudyUtils.isPageTabFile(fileMetaData.getAccession(), fileMetaData.getUiRequestedPath()) ) {
-            String url = indexConfig.getFtpOverHttpUrl(fileMetaData.getStorageMode());
-            response.sendRedirect( url +"/" + fileMetaData.getRelativePath() + "/Files/" + (fileMetaData.getUnDecodedRequestedPath()!=null?fileMetaData.getUnDecodedRequestedPath(): fileMetaData.getUiRequestedPath()) );
-            return true;
-        }
-        return false;
+public class FtpRedirectFilter implements FileChainFilter {
+  @Autowired private FireConfig fireConfig;
+  @Autowired private IndexConfig indexConfig;
+
+  @Override
+  public boolean handleFile(
+      FileMetaData fileMetaData, HttpServletRequest request, HttpServletResponse response)
+      throws Exception {
+    if (fireConfig.isFtpRedirectEnabled()
+        && fileMetaData.isPublic()
+        && !StudyUtils.isPageTabFile(
+            fileMetaData.getAccession(), fileMetaData.getUiRequestedPath())) {
+      String url = indexConfig.getFtpOverHttpUrl(fileMetaData.getStorageMode());
+      response.sendRedirect(
+          url
+              + "/"
+              + fileMetaData.getRelativePath()
+              + "/Files/"
+              + (fileMetaData.getUnDecodedRequestedPath() != null
+                  ? fileMetaData.getUnDecodedRequestedPath()
+                  : fileMetaData.getUiRequestedPath()));
+      return true;
     }
+    return false;
+  }
 }
